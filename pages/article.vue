082f74b770c0dfd7aa0851353113b61d2d878131
André <andre.amorim@prestashop.com>
2023-07-11 16:31:29 +0200

    initial commit

---
+++ pages/article.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>This is another statically rendered page with Nuxt</h1>
+        <NuxtLink to="/">Go back</NuxtLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>This is a statically rendered page with Nuxt</h1>
+        <NuxtLink to="/article">Go to existing page</NuxtLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
